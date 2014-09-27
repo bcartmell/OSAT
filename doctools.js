8a78cff8cc506673ab42522b7e5e91156e1d6edb
Brady Cartmell <bradycartmell+localgit@gmail.com>
2014-09-27 13:43:30 -0700

    improves doctools.js and introduces package.json

--- doctools.js
+++ doctools.js
@@ -1,8 +1,9 @@
 'use strict';
-var jsz = require('jszip');
-var fs = require('fs');
-var path = require('path');
-var dir = require('node-dir');
+var jsz = require('jszip'),
+    fs = require('fs'),
+    path = require('path'),
+    dir = require('node-dir'),
+    helpers = require('./helpers.js');
 
 /*
  *  OSATT FreeCad files handler
@@ -23,42 +24,46 @@ var dir = require('node-dir');
  *
  * */ 
 
-var freeCadHandler = {};
-
 function nameFile(source) {
-  console.log('source: '+ source);
   source = source.split('/');
-  console.log('source: '+ source);
   source = source[source.length-1];
-  console.log('source: '+ source);
   source = source.slice[0, source.indexOf('.')];
-  console.log('source: '+ source);
   return (source || "teapot_with_shapes") +".fcstd";
 }
 
 exports.create = function(source) {
   // resolve source
-  source = path.resolve(path.normalize(source));
-  var sourcePath = source.split('/');
+  source = helpers.resolveFile(source);
+
   // Define new zip object and set attributes.
   var fcstd = new jsz();
-  fcstd.name = nameFile(source)
+  console.log('finding the file name from '+ source);
+  fcstd.name = helpers.whatsFilename(source);
   fcstd.dir = true;
 
-  dir.readFiles(source, {shortName:true}, function(err, fileContent, fileName, next) {
-      if (err) throw err;
-      fcstd.file(fileName, fileContent, {compression:"DEFLATE"});  
-      next();
-    }, function(err, files) { // when finished
-      if (err) throw err;
-      // generate buffer for new file
-      var buffer = fcstd.generate({type:"nodebuffer"});
-      // write the file
-      fs.writeFile(fcstd.name, buffer, function(err) {
+  // Document.xml needs to be the first file in the folder.
+  fs.readFile(source+'/Document.xml', function(err, fileContent){
+    if (err) throw err;
+    fcstd.file('Document.xml', fileContent, {compression:"DEFLATE"});  
+
+    // Add the rest of the files
+    dir.readFiles(source, {shortName:true}, function(err, fileContent, fileName, next) {
         if (err) throw err;
-        console.log('new Fcstd created!');
+        if (fileName != 'Document.xml') {
+          fcstd.file(fileName, fileContent, {compression:"DEFLATE"});  
+        }
+        next();
+      }, function(err, files) { // when finished
+        if (err) throw err;
+        // generate buffer for new file
+        var buffer = fcstd.generate({type:"nodebuffer"});
+        // write the file
+        fs.writeFile(fcstd.name, buffer, function(err) {
+          if (err) throw err;
+          console.log('new Fcstd created!');
       }); // end writeFile()
-  }); // end dir.readFiles
+    }); // end dir.readFiles
+  });
 }; //  end create();
 
 exports.extract = function(source, destination) {
